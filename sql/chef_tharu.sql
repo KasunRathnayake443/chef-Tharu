6fbce71278b1a9a2141427a336041277202eeda2
Kasun Rathnayake <madksunmax@gmail.com>
2026-03-31 12:52:07 +0530

    db

--- sql/chef_tharu.sql
+++ sql/chef_tharu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2026 at 02:08 PM
+-- Generation Time: Mar 31, 2026 at 07:20 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
